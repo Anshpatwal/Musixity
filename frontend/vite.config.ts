55e409d217b1ada3050813ddfda06bc680fa7714
Anshpatwal <anshpatwal2703@gmail.com>
2025-02-25 09:31:54 +0530

    fixed port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0', // Allow external access
+    port: process.env.PORT ? Number(process.env.PORT) : 5173, // Convert PORT to a number
+  }
 })
